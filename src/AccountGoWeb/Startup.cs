3ac097412349a2ebfc5005d185baca3a6fe80b13
Ali Safder <alisafder@gmail.com>
2019-05-09 16:38:57 +0500

    First Commit

---
+++ src/AccountGoWeb/Startup.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.IO;
+using Infrastructure.AssemblyLoader;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.ApplicationParts;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AccountGoWeb
+{
+    public class Startup
+    {
+        public IConfigurationRoot Configuration { get; }
+
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+
+            string urlhost = System.Environment.GetEnvironmentVariable("APIHOST") ?? "localhost";
+            Configuration["ApiUrl"] = $"http://{urlhost}:8001/api/";
+            System.Console.WriteLine($"[ASPNETCORE SERVER] API URL {Configuration["ApiUrl"]}");
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var mvcBuilder = services
+            .AddMvc()
+            .SetCompatibilityVersion(Microsoft.AspNetCore.Mvc.CompatibilityVersion.Version_2_1);
+
+            services.AddSingleton<IConfiguration>(Configuration);
+
+            services.AddHttpClient();
+            
+            services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+                .AddCookie(o => o.LoginPath = new PathString("/account/signin"));
+
+            // Load and install modules. Get all module folder names inside 'Modules' folder
+            if (Directory.Exists(Path.Combine(AppContext.BaseDirectory, "modules")))
+            {
+                foreach (var dir in Directory.GetDirectories(Path.Combine(AppContext.BaseDirectory, "modules")))
+                {
+                    var moduleAssembly = new CustomAssemblyLoadContext().LoadFromAssemblyPath(Path.Combine(dir, Path.GetFileName(dir)) + ".dll");
+                    Console.WriteLine($"Loading application parts from module {moduleAssembly.FullName}");
+
+                    // This loads MVC application parts from module assemblies
+                    var partFactory = ApplicationPartFactory.GetApplicationPartFactory(moduleAssembly);
+                    foreach (var part in partFactory.GetApplicationParts(moduleAssembly))
+                    {
+                        Console.WriteLine($"* {part.Name}");
+                        mvcBuilder.PartManager.ApplicationParts.Add(part);
+                    }
+
+                    // This piece finds and loads related parts, such as SampleModule.Views.dll.
+                    var relatedAssemblies = RelatedAssemblyAttribute.GetRelatedAssemblies(moduleAssembly, throwOnError: true);
+                    foreach (var assembly in relatedAssemblies)
+                    {
+                        partFactory = ApplicationPartFactory.GetApplicationPartFactory(assembly);
+                        foreach (var part in partFactory.GetApplicationParts(assembly))
+                        {
+                            Console.WriteLine($"  * {part.Name}");
+                            mvcBuilder.PartManager.ApplicationParts.Add(part);
+                        }
+                    }
+                }
+            }
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseStaticFiles();
+
+            app.UseAuthentication();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
---
+++ src/Api/Startup.cs
@@ -0,0 +1,105 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+
+namespace Api
+{
+    public class Startup
+    {
+        private readonly IHostingEnvironment _hostingEnv;
+
+        public Startup(IHostingEnvironment env)
+        {
+            // Set up configuration sources.
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+            _hostingEnv = env;
+        }
+
+        public IConfigurationRoot Configuration { get; set; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            string connectionString = Configuration["Data:DevelopmentConnection:ConnectionString"];
+            // These environment variables can be overriden from launchSettings.json.
+            string dbServer = System.Environment.GetEnvironmentVariable("DBSERVER") ?? "localhost";
+            string dbUserID = System.Environment.GetEnvironmentVariable("DBUSERID") ?? "sa";
+            string dbUserPassword= System.Environment.GetEnvironmentVariable("DBPASSWORD") ?? "123";
+            string dbName= System.Environment.GetEnvironmentVariable("DBNAME") ?? "accountgo";
+
+            connectionString = String.Format(Configuration["Database:ConnectionString"], dbServer, dbUserID, dbUserPassword, dbName);
+
+            System.Console.WriteLine("DB Connection String: " + connectionString);
+
+            services
+                .AddEntityFrameworkSqlServer()
+                .AddDbContext<Data.ApiDbContext>(options => options.UseSqlServer(connectionString))
+                .AddDbContext<Data.ApplicationIdentityDbContext>(options => options.UseSqlServer(connectionString));
+
+            services
+                .AddIdentity<Data.ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<Data.ApplicationIdentityDbContext>()
+                .AddDefaultTokenProviders();
+
+            // Add framework services.
+            services.AddMvc()
+                .AddJsonOptions(options =>
+                {
+                    options.SerializerSettings.ContractResolver = new Newtonsoft.Json.Serialization.CamelCasePropertyNamesContractResolver();
+                    options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+                });
+
+            //services.AddSwaggerGen();
+
+            // Add cors
+            services.AddCors(o => o.AddPolicy("AllowAll", builder =>
+            {
+                builder
+                .AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader();
+            }));
+
+            // generic repository
+            services.AddScoped(typeof(Core.Data.IRepository<>), typeof(Data.EfRepository<>));
+
+            // custom repositories
+            services.AddScoped(typeof(Core.Data.ISalesOrderRepository), typeof(Data.SalesOrderRepository));
+            services.AddScoped(typeof(Core.Data.IPurchaseOrderRepository), typeof(Data.PurchaseOrderRepository));
+            services.AddScoped(typeof(Core.Data.ISecurityRepository), typeof(Data.Repositories.SecurityRepository));
+
+            // domain services
+            services.AddScoped(typeof(Services.Sales.ISalesService), typeof(Services.Sales.SalesService));
+            services.AddScoped(typeof(Services.Financial.IFinancialService), typeof(Services.Financial.FinancialService));
+            services.AddScoped(typeof(Services.Inventory.IInventoryService), typeof(Services.Inventory.InventoryService));
+            services.AddScoped(typeof(Services.Purchasing.IPurchasingService), typeof(Services.Purchasing.PurchasingService));
+            services.AddScoped(typeof(Services.Administration.IAdministrationService), typeof(Services.Administration.AdministrationService));
+            services.AddScoped(typeof(Services.Security.ISecurityService), typeof(Services.Security.SecurityService));
+            services.AddScoped(typeof(Services.TaxSystem.ITaxService), typeof(Services.TaxSystem.TaxService));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+                      
+            app.UseStaticFiles();
+            app.UseAuthentication();
+            app.UseCors("AllowAll");
+            app.UseMvc();
+            //app.UseSwaggerGen();
+            //app.UseSwaggerUi();
+        }        
+    }
+}
